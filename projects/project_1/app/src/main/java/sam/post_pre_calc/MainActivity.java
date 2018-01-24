0923f8c0ed2758a7a6275908d85bafaed77b9d8d
gdrjk <arching0@gmail.com>
2018-01-24 14:49:27 -0800

    Update MainActivity.java
    With this update, numeric digits are now calling one function, operands another. Opposed to each button having its own function.

--- projects/project_1/app/src/main/java/sam/post_pre_calc/MainActivity.java
+++ projects/project_1/app/src/main/java/sam/post_pre_calc/MainActivity.java
@@ -1,10 +1,8 @@
 /*
 This application is a post-pre calculator.
-
 AUTHOR: Samuel Alston
-DATE: DD/MM/YYYY 22/1/2018
+DATE MODIFIED: DD/MM/YYYY 24/1/2018
 CS 480 SP18
-
 OVERVIEW: This application is to complete the CS 480 Mobile Apps project 1 requirement:
         "I expect this application to be in the realm of complexity of a basic postfix
          calculator – one or more buttons that are tied to some sort of interaction with
@@ -22,92 +20,39 @@ package sam.post_pre_calc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void buttonOneOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("1");
-        //msgTextView.append("1");
+    private String bufferString="0";
+    private String finalDisplayString= "";
+
+    //operators call buttonOpOnClick
+    public void buttonOpOnClick(View v) {
+        TextView msgTextView = findViewById(R.id.buffer_screen);
+        Button clicked = findViewById(v.getId());
+        msgTextView.setText(clicked.getText());
     }
 
-    public void buttonTwoOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("2");
-        //msgTextView.append("1");
-    }
-    public void buttonThreeOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("3");
-        //msgTextView.append("1");
-    }
-    public void buttonFourOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("4");
-        //msgTextView.append("1");
-    }
-    public void buttonFiveOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("5");
-        //msgTextView.append("1");
-    }
-    public void buttonSixOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("6");
-        //msgTextView.append("1");
-    }
-    public void buttonSevenOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("7");
-        //msgTextView.append("1");
-    }
-    public void buttonEightOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("8");
-        //msgTextView.append("1");
-    }
-    public void buttonNineOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("9");
-        //msgTextView.append("1");
-    }
-    public void buttonZeroOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("0");
-        //msgTextView.append("1");
-    }
-    public void buttonPlusOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("+");
-        //msgTextView.append("1");
-    }
-    public void buttonMinusOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("-");
-        //msgTextView.append("1");
-    }
-    public void buttonMultiplyOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("*");
-        //msgTextView.append("1");
-    }
-    public void buttonDivideOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("/");
-        //msgTextView.append("1");
-    }
-    public void buttonPeriodOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText(".");
-        //msgTextView.append("1");
-    }
-    public void buttonEqualsOnClick(View v){
-        TextView msgTextView = findViewById(R.id.display_screen);
-        msgTextView.setText("=");
-        //msgTextView.append("1");
+    private void moveBufferToDisplay(){
+        finalDisplayString+=bufferString+" ";
+        TextView finalDisplayView = findViewById(R.id.final_display_screen);
+        finalDisplayView.setText(finalDisplayString);
     }
 
+    //Numerics call buttonNumericOnClick
+    public void buttonNumericOnClick(View v) {
+        TextView msgTextView = findViewById(R.id.buffer_screen);
+        Button clicked = findViewById(v.getId());
+
+        if( bufferString=="0"){
+            bufferString = (String)clicked.getText();
+        }else{
+            bufferString+=clicked.getText();
+        }
+        msgTextView.setText(bufferString);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
