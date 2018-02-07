959126182b61b3787c7a130cb3f43203f320a880
Sam Alston <arching0@gmail.com>
2018-02-07 14:45:22 -0800

    Update MainActivity.java
    This iteration can open images from the gallery and put them into imageView as well as load one from camera. Edit image button added, not hooked up to anything.

--- projects/project 2/source/java/alston/sam/photoapp/MainActivity.java
+++ projects/project 2/source/java/alston/sam/photoapp/MainActivity.java
@@ -1,34 +1,41 @@
 /*
 Author: Samuel Alston
-Last Modified: dd/mm/yyyy 05/02/2018
-
+Last Modified: dd/mm/yyyy 07/02/2018
 Project Overview:
     The goal of this app is to meet the standards of the CS 480 Project #2 requirements.
     "...develop an image manipulation application. Your program should be able to both
     manipulate existing phone images or to take new ones that can then be manipulated."
-
 Current Status: Currently, the application launches to the first view where you can
     press a button to launch the camera, once you take an image and approve it said
     image will appear in the imageView below the open camera button in the first view.
-    Open image gallery button added, does not yet add image to imageView.
+    Open image gallery button added, selected image can be moved to the imageView
+    Edit image button added. Nothing attached to it yet.
 */
 package alston.sam.photoapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
+
+import java.io.InputStream;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnpic;
     Button btngal;
     ImageView imgTakenPic;
-    private static final int CAM_REQUEST=1313;
+    //code for using camera
+    private static final int CAM_REQUEST = 1313;
+    //code for using gallery/file explorer
     public static final int PICK_IMAGE = 1;
 
     @Override
@@ -49,14 +56,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         try{
             super.onActivityResult(requestCode, resultCode, data);
-
+            //if cam request, get image from camera
             if(requestCode == CAM_REQUEST){
                 Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                 imgTakenPic.setImageBitmap(bitmap);
             }
-            if(requestCode == PICK_IMAGE) {
-                Bitmap bitmap =  (Bitmap) data.getExtras().get("data");
-                imgTakenPic.setImageBitmap(bitmap);
+            //if picking image from gallery we have to convert URI to data via inputStream
+            if(requestCode == PICK_IMAGE && resultCode == RESULT_OK && null != data) {
+               Uri imageUri = data.getData();
+               InputStream imageStream = getContentResolver().openInputStream(imageUri);
+               Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+               imgTakenPic.setImageBitmap(selectedImage);
             }
         } catch(Exception e) {
             //don't break if the user sends no image
@@ -65,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class btnTakePhotoClicker implements Button.OnClickListener{
-
+        //set Open camera button intent
         @Override
         public void onClick(View view){
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -74,13 +84,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class btnOpenGalleryClicker implements  Button.OnClickListener{
-
+        //set Open Gallery button intent
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            // Create intent to Open Image applications like Gallery, Google Photos
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            // Start the Intent
+            startActivityForResult(galleryIntent, PICK_IMAGE);
+
         }
 
     }
